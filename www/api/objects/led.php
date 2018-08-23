1e72766c8664b0d553c3d6d40ed9ab6587cc274a
consocio <frabuscaino@gmail.com>
2018-08-23 12:36:54 +0200

    test

--- www/api/objects/led.php
+++ www/api/objects/led.php
@@ -1,16 +1,16 @@
 <?php
 class Led{
- 
+
     // database connection and table name
     private $conn;
     private $table_name = "led";
- 
+
     // object properties
     public $id;
     public $status;
-    public $ttime;
-    public $ttdate;
- 
+    public $time;
+    public $tdate;
+
     // constructor with $db as database connection
     public function __construct($db){
         $this->conn = $db;
